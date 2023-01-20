dda482fa46dd8f43e5cae0e4e54b446e96e71ae3
tyckobra <77719515+tyckobra@users.noreply.github.com>
2023-01-20 08:47:59 +0100

    23-01-20

--- src/Model/Cell.java
+++ src/Model/Cell.java
@@ -2,12 +2,26 @@ package Model;
 
 import View.Point;
 
+import java.awt.*;
+
 public class Cell {
-private boolean isAlive;
-private Point position;
+    private boolean isAlive;
+    private Point position;
+
     public Cell(int w, int h) {
         isAlive = true;
-        position = new Point();
+        position = new Point(w,h);
     }
 
+    public void update() {
+
+    }
+
+    public boolean isAlive() {
+        return isAlive;
+    }
+
+    public Point getPoint() {
+        return position;
+    }
 }
--- src/Model/Model.java
+++ src/Model/Model.java
@@ -1,33 +1,36 @@
 package Model;
 import Model.Cell;
 import View.*;
-import java.awt.*;
-import java.awt.Shape;
+//import java.awt.*;
+//import java.awt.Shape;
 import java.util.ArrayList;
 
 public class Model {
 
     int width;
     int height;
-    Cell = cell;
+    Cell c;
 
     public Model(int w, int h) {
         this.width = w;
         this.height = h;
-        this.cell = new Cell(10,10);
+        this.c = new Cell(1,1);
     }
 
     public void update() {
-        cell.update();
+        c.update();
     }
 
     public Shape[] getShapes() {
-        if (cell.isAlive()) {
-            Shape[] points = new Shape[1];
-            points[0] = cell.getPoint();
-            return points;
-        } else {
-            return new Shape[0];
+        ArrayList<Point> points = new ArrayList<Point>();
+
+        if (c.isAlive()) {
+            points.add(c.getPoint());
         }
+
+        Point[] pixelsToDisplay = new Point[points.size()];
+        points.toArray(pixelsToDisplay);
+
+        return pixelsToDisplay;
     }
 }
